8ab68767e87de42972d718abf1ca06403f00f2e7
realflash2 <realflash2@60f25ad5-ef42-bdeb-fb0c-bd16c7393de6>
2011-07-07 07:24:23 +0000

    Created tag v0.2.1.

---
+++ tags/v0.2.1/build/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+DIR="HTMLTemplate"
+NAME="html-template-ps"
+VERSION="0.2.1"
+OUT="$NAME-v$VERSION.zip"
+
+function make
+{
+	if [[ ! -d $DIR ]]; then
+	{
+		mkdir $DIR
+	}
+	else
+	{
+		rm -rf $DIR
+	}
+	fi
+
+	cp -r ../source/* $DIR
+	cp -r ../aux/* $DIR
+
+	zip -9 $OUT $DIR
+
+}
+
+function clean
+{
+	rm -rf $DIR
+	rm -f $OUT
+}
+
+if [[ "$1" == "clean" || "$1" == "CLEAN" ]]; then
+{
+	clean
+}
+else
+{
+	clean
+	make
+}
+fi
